1eef87d3007fbcecb353b5d9a8aeb342830ebb21
letrungkien0210 <letrungkien0210@gmail.com>
2016-04-08 10:42:49 +0700

    Update app.js

---
+++ BuildRESTfulAPI/view/js/app.js
@@ -0,0 +1,18 @@
+//name our angular app
+angular.module('firstApp',[])
+  .controller('mainController', function(){
+    //bind this to vm (view-model)
+    var vm = this;
+    
+    //define variables and objects on this
+    //this lets them be available to our view
+    //define a basic variable
+    vm.message = 'Hey there! Come and see how good I look!';
+    
+    //define a list of items
+    vm.computers = [
+      {name: 'Macbook Pro', color: 'Silver', nerdness: 7},
+      {name: 'Yora 2 Pro', color: 'Gray', nerdness: 6},
+      {name: 'Chromebook', color: 'Black', nerdness: 5},
+    ];
+  });
