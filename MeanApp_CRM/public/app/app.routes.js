57069811e2618cae4fde747da381a82a593ead46
letrungkien0210 <letrungkien0210@gmail.com>
2016-04-21 21:51:18 +0700

    12

--- MeanApp_CRM/public/app/app.routes.js
+++ MeanApp_CRM/public/app/app.routes.js
@@ -10,6 +10,21 @@ angular.module('app.routes',['ngRoute'])
 			templateUrl: 'app/views/pages/login.html',
 			controller: 'mainController',
 			controllerAs: 'login'
+		})
+		.when('/users',{
+			templateUrl: 'app/views/pages/all.html',
+			controller: 'userController',
+			controllerAs: 'user'
+		})
+		.when('/users/create',{
+			templateUrl: 'app/views/pages/users/single.html',
+			controller: 'userCreateController',
+			controllerAs: 'user'
+		})
+		.when('/users/:user_id',{
+			templateUrl: 'app/views/pages/user/single.html',
+			controller: 'userEditController',
+			controllerAs: 'user'
 		});
 		
 		//get rid of the hash in the URL
