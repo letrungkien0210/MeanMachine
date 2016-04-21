bc150f0614c5e9d2668b1e879797999826b1ae0c
letrungkien0210 <letrungkien0210@gmail.com>
2016-04-21 10:30:49 +0700

    Update app.js

--- MeanApp_CRM/public/app/app.js
+++ MeanApp_CRM/public/app/app.js
@@ -5,4 +5,9 @@ angular.module('userApp',[
 	'mainCtrl',
 	'userCtrl',
 	'userService'
-]);
+])
+//application configuration to integrate token into requests
+.config(function($httpProvider){
+	//attch our auth interceptor to the http requests
+	$httpProvider.interceptor.push('AuthIntercaptor');
+});
