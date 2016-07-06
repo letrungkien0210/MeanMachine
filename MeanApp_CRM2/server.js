d6b37a95c16fa425de5955d176b6907e753af1fd
letrungkien02101 <letrungkien0210@outlook.com>
2016-07-06 22:40:22 +0000

    Catchall Route

--- MeanApp_CRM2/server.js
+++ MeanApp_CRM2/server.js
@@ -1,4 +1,5 @@
 var express = require("express");
+var path = require('path');
 var app = express();
 var mongoose = require("mongoose");
 var config = require('./config.js');
@@ -9,6 +10,13 @@ mongoose.connect(config.database);
 //API ROUTES
 var apiRouter = require('./app/routes/api')(app, express);
 app.use('/api', apiRouter);
+
+//MAIN CATCH ROUTE-------------------------
+//SEND USER TO FRONTEND--------------------
+//has to be registered after API ROUTES
+app.get('*',function(req, res){
+    res.sendFile(path.join(__dirname + '/public/app/views/index.html'));
+})
 
 //START THE SERVER 
 //=========================================
