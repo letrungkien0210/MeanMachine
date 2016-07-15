8adc7f554fb36961cac7fb37e6f2c2df3ce07b69
letrungkien0210 <letrungkien0210@gmail.com>
2016-07-15 07:09:40 +0700

    Create User - Create User controller

--- MeanApp_CRM2/public/app/app.routes.js
+++ MeanApp_CRM2/public/app/app.routes.js
@@ -9,6 +9,11 @@ angular.module('app.routes', ['ngRoute'])
         templateUrl: 'app/views/pages/login.html',
         controller: 'mainController',
         controllerAs: 'login'
+    })
+    .when('/users', {
+        templateUrl: 'app/views/pages/users/all.html',
+        controller: 'userController',
+        controllerAs: 'user'
     });
 
     //get rid of the hash in the URL
